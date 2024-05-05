157df2b6d71709e21bc1cc09659b21a132a6dba8
Isadora Dantas <isadoramariadasilvadantas@gmail.com>
2024-05-05 13:58:37 -0300

    first commit

---
+++ tests/Fixture/TagsFixture.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * TagsFixture
+ */
+class TagsFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'title' => 'Lorem ipsum dolor sit amet',
+                'created' => '2024-05-05 16:26:20',
+                'modified' => '2024-05-05 16:26:20',
+            ],
+        ];
+        parent::init();
+    }
+}
